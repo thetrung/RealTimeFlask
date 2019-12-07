1fba88e10368ff8cd986186ee8fcdf03c1282f52
Trung Nguyen The <thetrung@Trungs-MacBook-Pro.local>
2019-12-07 19:18:12 +0700

    Init working demo

---
+++ static/js/application.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+
+    //connect to the socket server.
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    var numbers_received = [];
+    var numbers_labels = [];
+
+
+    // Global parameters:
+    // do not resize the chart canvas when its container does (keep at 600x400px)
+    Chart.defaults.global.responsive = true;
+
+    // get chart canvas
+    var ctx = document.getElementById("myChart").getContext("2d");
+
+    // define the chart data
+    var chartData = {
+    labels : numbers_received,
+    datasets : [{
+        label: "data",
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data : numbers_received,
+        spanGaps: false
+    }]
+    }
+
+    // create the chart using the chart canvas
+    var myChart = new Chart(ctx, {
+    type: 'line',
+    data: chartData,
+    });
+
+    myChart.options.duration = 0;
+
+
+    function update_chart(chart, labels, data) {
+      chart.data.labels = labels;
+      chart.data.datasets.data = data;
+      chart.update();
+    }
+    //receive details from server
+    socket.on('newnumber', function(msg) {
+        console.log("Received number" + msg.number);
+        //maintain a list of ten numbers
+        if (numbers_received.length >= 50){
+            numbers_received.shift();
+            numbers_labels.shift();
+        }
+        numbers_received.push(msg.number);
+        numbers_labels.push('#' + msg.number);
+
+        numbers_string = '';
+        for (var i = 0; i < numbers_received.length; i++){
+            // numbers_labels.push(i);
+            numbers_string = numbers_string + '<p>' + numbers_received[i].toString() + '</p>';
+        }
+
+        update_chart(myChart, numbers_labels, numbers_received)
+        $('#log').html(numbers_string);
+
+
+    });
+
+
+
+});
